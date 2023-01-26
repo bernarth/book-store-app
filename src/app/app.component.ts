5919235283a1c436b9cc1765c9c58fde185a8973
Bernarth <alberthbernarth75@gmail.com>
2023-01-26 00:31:32 -0400

    Change to templateUrl and styleUrls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-root',
-    template: '<h1>Hello Template</h1>',
-    styles: ['h1{color:blue}']
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {}
