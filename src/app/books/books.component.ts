7da7dbb6116e85bbbf7b2295c50a4e5ae6d38b7a
Bernarth <alberthbernarth75@gmail.com>
2023-01-28 18:21:06 -0400

    Add ngFor directive

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 
+// It's good to define the type so interfaces helps with that.
+interface Book {
+  name: string;
+  author: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
-  name: string = 'Clean Code';
-  author: string = 'Roberth C Martin';
-  src: string = 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
-
-  name2: string = 'The Pragmatic Programmer';
-  author2: string = 'David Thomas';
-  src2: string = 'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg';
-
+  books: Book[] = [
+    {
+      name: 'Clean Code',
+      author: 'Roberth C Martin',
+      image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg'
+    },
+    {
+      name: 'The Pragmatic Programmer',
+      author: 'David Thomas',
+      image: 'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg'
+    }
+  ];
+  
   isShowing: boolean = true;
 
   constructor() {}
