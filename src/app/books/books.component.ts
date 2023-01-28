43ea55544580d027c3176e2d61f4056b4b2575d3
Bernarth <alberthbernarth75@gmail.com>
2023-01-28 18:33:12 -0400

    Add Pipes concept

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -5,6 +5,7 @@ interface Book {
   name: string;
   author: string;
   image: string;
+  amount: number;
 }
 
 @Component({
@@ -15,14 +16,16 @@ interface Book {
 export class BooksComponent {
   books: Book[] = [
     {
-      name: 'Clean Code',
-      author: 'Roberth C Martin',
-      image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg'
+      name: 'clean code',
+      author: 'roberth c martin',
+      image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+      amount: 700
     },
     {
-      name: 'The Pragmatic Programmer',
-      author: 'David Thomas',
-      image: 'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg'
+      name: 'the pragmatic programmer',
+      author: 'david thomas',
+      image: 'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg',
+      amount: 800
     }
   ];
   
